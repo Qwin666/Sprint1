162d41351b678a6673534656faa081008732c3f6
Nikita <istefa92@gmail.com>
2022-04-19 19:54:47 +0700

    short url

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -17,23 +18,33 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Привет"))
+	w.Write([]byte("Home"))
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-
-	}
-	for key, v := range Bd {
-		if id == key {
-			http.Redirect(w, r, v, http.StatusFound)
-
+	switch r.Method {
+	case http.MethodGet:
+		id, err := strconv.Atoi(r.URL.Query().Get("id"))
+		if err != nil || id < 1 {
+			http.NotFound(w, r)
 		}
-	}
+		for key, v := range Bd {
+			if id == key {
+				http.Redirect(w, r, v, http.StatusFound)
 
-	http.NotFound(w, r)
-	return
+			}
+		}
+
+		http.NotFound(w, r)
+		return
+	case http.MethodPost:
+		fmt.Println("post")
+
+	default:
+		fmt.Fprintf(w, "поддерживает только  GET и POST методы")
+		http.Error(w, "поддерживает только  GET и POST методы", http.StatusBadRequest)
+
+	}
 }
 
 func handleFunc() {
